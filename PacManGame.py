f53fae44dd957b0acb44251e02241d1cf050768e
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-02-11 22:33:59 +0200

    Creating interaction LevelBuilder with PacManGame

--- PacManGame.py
+++ PacManGame.py
@@ -1,5 +1,34 @@
 import pygame
+from win32api import GetSystemMetrics
+
+import LevelMap
+from LevelBuilder import LevelBuilder
+
 
 class PacManGame:
+    HEIGHT = GetSystemMetrics(1) - 70
+    WIDTH = HEIGHT * 0.95
+    FPS = 60
+
     def start_game(self):
-        pass
+        pygame.init()
+        screen = pygame.display.set_mode([self.WIDTH, self.HEIGHT])
+        timer = pygame.time.Clock()
+
+        running = True
+
+        level_controller = LevelBuilder(screen, self.WIDTH, self.HEIGHT, LevelMap.boards).build()
+
+        while running:
+            timer.tick(self.FPS)
+            screen.fill("black")
+
+            level_controller.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            pygame.display.flip()
+
+        pygame.quit()
