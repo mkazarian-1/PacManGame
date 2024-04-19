9a4afe45916a9adeafeffb165bbb925ec80cfab1
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-04-19 09:51:04 +0300

    PacManTest update

--- test/test_PacMan.py
+++ test/test_PacMan.py
@@ -4,6 +4,7 @@ import pytest
 from src import Score
 from src.Health import Health
 from src.PacMan import PacMan
+from src.Position import Position
 from src.level import LevelMap, LevelBuilder, LevelEnvironment
 from src.level.EndGameController import EndGameController
 
@@ -44,6 +45,112 @@ def test_is_cell_action_with_non_actionable_cell(pacman):
     cell = LevelEnvironment.Wall(None, None, None, None, None, None, None)
 
     assert pacman.is_cell_action(cell) is False
+
+
+def test_pacman_rotation_turn_right(pacman):
+    pacman.turn = Position.RIGHT
+    direction = pacman.pacman_rotation(Position.LEFT, [True, False, False, False])
+    assert direction == Position.RIGHT
+
+
+def test_pacman_rotation_turn_left(pacman):
+    pacman.turn = Position.LEFT
+    direction = pacman.pacman_rotation(Position.RIGHT, [False, True, False, False])
+    assert direction == Position.LEFT
+
+
+def test_pacman_rotation_turn_up(pacman):
+    pacman.turn = Position.UP
+    direction = pacman.pacman_rotation(Position.DOWN, [False, False, True, False])
+    assert direction == Position.UP
+
+
+def test_pacman_rotation_turn_down(pacman):
+    pacman.turn = Position.DOWN
+    direction = pacman.pacman_rotation(Position.UP, [False, False, False, True])
+    assert direction == Position.DOWN
+
+
+def test_pacman_rotation_no_turn(pacman):
+    pacman.turn = Position.UP
+    direction = pacman.pacman_rotation(Position.DOWN, [False, False, False, False])
+    assert direction == Position.DOWN
+
+
+def test_out_of_bound_controller_right_boundary(pacman):
+    direction = Position.RIGHT
+    pacman_cell_x = pacman.cell_len_x - 2
+    pacman_cell_y = 5
+    new_x, new_y = pacman.out_of_bound_controller(direction, pacman_cell_x, pacman_cell_y)
+    assert new_x == 0
+
+
+def test_out_of_bound_controller_left_boundary(pacman):
+    direction = Position.LEFT
+    pacman_cell_x = 0
+    pacman_cell_y = 5
+    new_x, new_y = pacman.out_of_bound_controller(direction, pacman_cell_x, pacman_cell_y)
+    assert new_x == pacman.cell_len_x - 2
+
+
+def test_out_of_bound_controller_top_boundary(pacman):
+    direction = Position.UP
+    pacman_cell_x = 5
+    pacman_cell_y = pacman.cell_len_y - 2
+    new_x, new_y = pacman.out_of_bound_controller(direction, pacman_cell_x, pacman_cell_y)
+    assert new_y == 0
+
+
+def test_out_of_bound_controller_bottom_boundary(pacman):
+    direction = Position.DOWN
+    pacman_cell_x = 5
+    pacman_cell_y = 0
+    new_x, new_y = pacman.out_of_bound_controller(direction, pacman_cell_x, pacman_cell_y)
+    assert new_y == pacman.cell_len_y - 2
+
+
+def test_out_of_bound_controller_no_change(pacman):
+    direction = Position.RIGHT
+    pacman_cell_x = 5
+    pacman_cell_y = 5
+    new_x, new_y = pacman.out_of_bound_controller(direction, pacman_cell_x, pacman_cell_y)
+    assert new_x == pacman_cell_x
+    assert new_y == pacman_cell_y
+
+
+def test_is_out_of_bounds_right_boundary(pacman):
+    direction = Position.RIGHT
+    pacman_cell_x = pacman.cell_len_x - 2
+    pacman_cell_y = 5
+    assert pacman.is_out_of_bounds(direction, pacman_cell_x, pacman_cell_y) is True
+
+
+def test_is_out_of_bounds_left_boundary(pacman):
+    direction = Position.LEFT
+    pacman_cell_x = 0
+    pacman_cell_y = 5
+    assert pacman.is_out_of_bounds(direction, pacman_cell_x, pacman_cell_y) is True
+
+
+def test_is_out_of_bounds_top_boundary(pacman):
+    direction = Position.UP
+    pacman_cell_x = 5
+    pacman_cell_y = pacman.cell_len_y - 2
+    assert pacman.is_out_of_bounds(direction, pacman_cell_x, pacman_cell_y) is True
+
+
+def test_is_out_of_bounds_bottom_boundary(pacman):
+    direction = Position.DOWN
+    pacman_cell_x = 5
+    pacman_cell_y = 0
+    assert pacman.is_out_of_bounds(direction, pacman_cell_x, pacman_cell_y) is True
+
+
+def test_is_out_of_bounds_no_boundary(pacman):
+    direction = Position.RIGHT
+    pacman_cell_x = 5
+    pacman_cell_y = 5
+    assert pacman.is_out_of_bounds(direction, pacman_cell_x, pacman_cell_y) is False
 
 # def test_is_cell_action_with_energiser(pacman_instance):
 #     # Перевірка, чи правильно визначається дія для клітинки з енергійними стимуляторами
